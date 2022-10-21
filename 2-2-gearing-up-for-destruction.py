edf65002ed5ba93be675b0b4548eeb875a09f05c
asad.abrar <syed.abrar@careem.com>
2022-10-21 21:24:29 +0500

    Minor changes

--- 2-2-gearing-up-for-destruction.py
+++ 2-2-gearing-up-for-destruction.py
@@ -6,6 +6,22 @@ def solution(pegs):
 
   isEven = n % 2 == 0
 
+  multipler = -2
+
+  sum = 0
+
+  result = 1
+
+  for i in range(len(pegs)):
+    sum = (-1) ** (i) * pegs[i]
+
+    print(sum)
+
+    sum *= 2 if i != 0 and i != len(pegs) - 1 else 1
+
+  result *= multipler * (float(sum)/3 if isEven else sum)
+
+  print(result)
 
 
 print(solution([4, 30, 50]))
---
+++ 3-1-bomb-baby.py
@@ -0,0 +1,5 @@
+def solution(x, y):
+    # Your code here
+    print("start")
+
+print(solution('2', '1'))
