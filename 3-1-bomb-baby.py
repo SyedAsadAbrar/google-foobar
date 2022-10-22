fc928523e9319ccb7241fd4baef04f3702052428
Asad <syedasadabrar@gmail.com>
2022-10-22 05:15:57 +0500

    Final commit for bomb-baby

--- 3-1-bomb-baby.py
+++ 3-1-bomb-baby.py
@@ -1,5 +1,30 @@
-def solution(x, y):
+def solution(m, f):
     # Your code here
-    print("start")
+    startingTuple = (1, 1)
+    
+    goalTuple = (int(m), int(f))
+    
+    levels = 0
 
-print(solution('2', '1'))
+    currM, currF = goalTuple
+
+    while currM > 1 or currF > 1:
+        if (currF == currM and currF != 1) or currF < 1 or currM < 1:
+            return "impossible"
+        elif currM > currF:
+            levels += int(currM / currF)
+            currM = (currM % currF)
+        elif currM < currF:
+            levels += int(currF / currM)
+            currF = (currF % currM)
+
+    if (currF == 1 and currM == 0) or (currM == 1 and currF == 0):
+        return str(levels - 1)
+
+    if currM < 1 or currF < 1:
+        return "impossible"
+
+    if currF == 1 and currM == 1:
+        return str(levels)
+
+print(solution(2, 1))
