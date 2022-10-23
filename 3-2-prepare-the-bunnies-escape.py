a617cf154d006ebf27fbe7c6f60c6b710c926246
asad.abrar <syed.abrar@careem.com>
2022-10-23 18:03:14 +0500

    WIP: prepare-the-bunnies-escape

--- 3-2-prepare-the-bunnies-escape.py
+++ 3-2-prepare-the-bunnies-escape.py
@@ -1,6 +1,26 @@
+def printMatrix(arr):
+    for i in arr:
+        print(i)
+
+
 def solution(map):
     # Your code here
+    printMatrix(map)
+
     print("")
 
+    inverseMatrix = ([list[::-1] for list in map])[::-1]
 
-solution([])
+    printMatrix(inverseMatrix)
+
+    weightedInverseMatrix = []
+
+    # first iteration
+    for list in inverseMatrix:
+        for num in list:
+
+
+arr = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0],
+       [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
+
+solution(arr)
