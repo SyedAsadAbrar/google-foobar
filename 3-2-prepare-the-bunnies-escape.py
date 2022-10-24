303c6c69c80160815e8f350fac1c99b5cf61b2a2
Asad <syedasadabrar@gmail.com>
2022-10-25 02:00:56 +0500

    WIP: prepare-the-bunnies-escape

--- 3-2-prepare-the-bunnies-escape.py
+++ 3-2-prepare-the-bunnies-escape.py
@@ -1,10 +1,10 @@
 infinity = float('inf')
 
 
-def printMatrix(arr):
-    for i in arr:
-        print(i)
-    print("")
+# def printMatrix(arr):
+#     for i in arr:
+#         print(i)
+#     print("")
 
 
 def findNavigableNeighbours(position, matrix):
@@ -30,20 +30,40 @@ def findNavigableNeighbours(position, matrix):
     return navigableNeighbours
 
 
-def solution(map):
-    # Your code here
-    printMatrix(map)
+def findNeighbouringWallsCount(position, matrix):
+    cols = len(matrix)
+    rows = len(matrix[0])
 
-    matrix = [[num for num in arr] for arr in map]
+    col, row = position
 
-    lengths = [[infinity for num in arr] for arr in map]
+    count = 0
+
+    if col - 1 >= 0 and matrix[col - 1][row] == 1:
+        count += 1
+
+    if row - 1 >= 0 and matrix[col][row - 1] == 1:
+        count += 1
+
+    if col + 1 < cols and matrix[col + 1][row] == 1:
+        count += 1
+
+    if row + 1 < rows and matrix[col][row + 1] == 1:
+        count += 1
+
+    return count
+
+
+def findLeastNumberOfMoves(matrix):
+    cols = len(matrix)
+    rows = len(matrix[0])
+
+    lengths = [[infinity for _ in range(rows)] for _ in range(cols)]
     lengths[0][0] = 0
-    visited = [[False for num in arr] for arr in map]
+    visited = [[False for _ in range(rows)] for _ in range(cols)]
 
     temp = [(0, 0)]
 
     while len(temp) != 0:
-        print("temp", temp)
         col, row = temp[0]
         if not visited[col][row] and matrix[col][row] != 1:
             visited[col][row] = True
@@ -53,18 +73,49 @@ def solution(map):
                 lengths[neighbourCol][neighbourRow] = lengths[col][row] + 1 if lengths[col][row] + \
                     1 < lengths[neighbourCol][neighbourRow] else lengths[neighbourCol][neighbourRow]
             temp.extend(navigableNeigbours)
-            print("temp after append", temp)
         temp.pop(0)
 
-    printMatrix(matrix)
-    printMatrix(lengths)
-    printMatrix(visited)
+    return lengths[cols - 1][rows - 1] + 1
+
+
+def solution(matrix):
+    # Your code here
+    cols = len(matrix)
+    rows = len(matrix[0])
+
+    if cols >= 2 and cols <= 20 and rows >= 2 and cols <= 20:
+        result = []
+
+        possibleWallRemovingPositions = []
+
+        newMatrices = []
+
+        # printMatrix(matrix)
+
+        result.append(findLeastNumberOfMoves(matrix))
+
+        for col in range(cols):
+            for row in range(rows):
+                if matrix[col][row] == 1:
+                    value = findNeighbouringWallsCount((col, row), matrix)
+                    if value == 1:
+                        possibleWallRemovingPositions.append((col, row))
+
+        # printMatrix(possibleWallRemovingPositions)
+
+        for position in possibleWallRemovingPositions:
+            col, row = position
+            newMatrix = [[num for num in arr] for arr in matrix]
+            newMatrix[col][row] = 0
+            newMatrices.append(newMatrix)
+
+        for matrix in newMatrices:
+            result.append(findLeastNumberOfMoves(matrix))
+
+        return min(result)
 
 
 arr = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0],
        [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
 
-arr2 = [[0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0]]
-
-solution(arr)
+print(solution(arr))
