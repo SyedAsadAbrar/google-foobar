fee6b1bc2659a2dcd4fdfd5d6d154e217d1a5c71
Asad <syedasadabrar@gmail.com>
2022-10-31 02:06:51 +0500

    WIP: doomsday-fuel

--- 3-3-doomsday-fuel.py
+++ 3-3-doomsday-fuel.py
@@ -8,6 +8,43 @@ def printMatrix(m):
     print("")
 
 
+def eliminate(r1, r2, col, target=0):
+    fac = (r2[col]-target) / r1[col]
+    for i in range(len(r2)):
+        r2[i] -= fac * r1[i]
+
+
+def gauss(a):
+    for i in range(len(a)):
+        if a[i][i] == 0:
+            for j in range(i+1, len(a)):
+                if a[i][j] != 0:
+                    a[i], a[j] = a[j], a[i]
+                    break
+            else:
+                raise ValueError("Matrix is not invertible")
+        for j in range(i+1, len(a)):
+            eliminate(a[i], a[j], i)
+    for i in range(len(a)-1, -1, -1):
+        for j in range(i-1, -1, -1):
+            eliminate(a[i], a[j], i)
+    for i in range(len(a)):
+        eliminate(a[i], a[i], i, target=1)
+    return a
+
+
+def inverse(a):
+    tmp = [[] for _ in a]
+    for i, row in enumerate(a):
+        assert len(row) == len(a)
+        tmp[i].extend(row + [0]*i + [1] + [0]*(len(a)-i-1))
+    gauss(tmp)
+    ret = []
+    for i in range(len(tmp)):
+        ret.append(tmp[i][len(tmp[i])//2:])
+    return ret
+
+
 def solution(m):
     # Your code here
     matrix = [[(Fraction(num, sum(arr)) if (sum(
@@ -54,7 +91,7 @@ def solution(m):
 
     n = (i - q)
 
-    n = np.linalg.inv(n)
+    n = inverse(n)
 
     result = np.dot(n, r)
 
