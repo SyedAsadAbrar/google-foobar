ea07a0537283aac4bacaee63a11392b5f5d18596
Asad <syedasadabrar@gmail.com>
2022-10-30 18:11:26 +0500

    WIP: doomsday-fuel

--- 3-3-doomsday-fuel.py
+++ 3-3-doomsday-fuel.py
@@ -1,47 +1,5 @@
 from fractions import Fraction
-
-
-def eliminate(r1, r2, col, target=0):
-    fac = (r2[col]-target) / r1[col]
-    for i in range(len(r2)):
-        r2[i] -= fac * r1[i]
-
-
-def gauss(a):
-    for i in range(len(a)):
-        if a[i][i] == 0:
-            for j in range(i+1, len(a)):
-                if a[i][j] != 0:
-                    a[i], a[j] = a[j], a[i]
-                    break
-            else:
-                raise ValueError("Matrix is not invertible")
-        for j in range(i+1, len(a)):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)-1, -1, -1):
-        for j in range(i-1, -1, -1):
-            eliminate(a[i], a[j], i)
-    for i in range(len(a)):
-        eliminate(a[i], a[i], i, target=1)
-    return a
-
-
-def inverse(a):
-    tmp = [[] for _ in a]
-    for i, row in enumerate(a):
-        assert len(row) == len(a)
-        tmp[i].extend(row + [0]*i + [1] + [0]*(len(a)-i-1))
-    gauss(tmp)
-    ret = []
-    for i in range(len(tmp)):
-        ret.append(tmp[i][len(tmp[i])//2:])
-    return ret
-
-
-def multiply(x, y):
-    result = [[sum(Fraction(a*b).limit_denominator() for a, b in zip(X_row, Y_col))
-               for Y_col in zip(*y)] for X_row in x]
-    return result
+import numpy as np
 
 
 def lcm(arr):
@@ -86,12 +44,8 @@ def solution(m):
 
     for index in range(len(matrix)):
         arr = matrix[index]
-        isZero = True
-        for num in arr:
-            if num != 0:
-                isZero = False
-                break
-        if isZero:
+        arrSum = sum(arr)
+        if arrSum == 0:
             if index == 0:
                 result = [1] + [num for num in arr[1:]]
                 result.append(1)
@@ -114,30 +68,18 @@ def solution(m):
         for innerIndex in range(len(matrix) - count, len(matrix)):
             r[outerIndex].append(arr[innerIndex])
 
-    # make identity matrix
-    for outerIndex in range(count):
-        i.append([])
-        for innerIndex in range(count):
-            if innerIndex == outerIndex:
-                i[outerIndex].append(1)
-            else:
-                i[outerIndex].append(0)
+    q = np.matrix(q, dtype=float)
 
-    for outerIndex in range(len(q)):
-        n.append([])
-        for innerIndex in range(len(q[outerIndex])):
-            difference = i[outerIndex][innerIndex] - q[outerIndex][innerIndex]
-            n[outerIndex].append(difference)
+    i = np.identity(len(q))
 
-    n = inverse(n)
+    n = i - q
 
-    for outerIndex in range(len(n)):
-        for innerIndex in range(len(n[outerIndex])):
-            n[outerIndex][innerIndex] = n[outerIndex][innerIndex]
+    n = np.linalg.inv(n)
 
-    result = multiply(n, r)
+    result = np.dot(n, r)
 
-    print(result)
+    result = [[Fraction(decimal).limit_denominator() for decimal in arr]
+              for arr in np.asarray(result)]
 
     denominator = lcm(
         [fraction.denominator for fraction in result[0]])
@@ -161,4 +103,4 @@ arr = [[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [
 arr2 = [[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]
 
-print(solution(testCase))
+print(solution(arr))
