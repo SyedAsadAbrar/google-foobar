9ed59c12cb0d592ce0e848c0b43f4c3cb5bded1a
Asad <syedasadabrar@gmail.com>
2022-11-07 02:26:52 +0500

    free-the-bunny-workers done

--- 4-1-free-the-bunny-workers.py
+++ 4-1-free-the-bunny-workers.py
@@ -7,16 +7,18 @@ def solution(num_buns, num_required):
     if num_required < 0 or num_required > 9:
         return []
 
-    perm = permutations(range(num_buns), num_required)
-    comb = combinations(range(num_buns), num_required)
+    # finding out possible combinations of num_buns taken num_required at a time
+    # these combinations would be the possible groups of bunnies which get a key x
+    possibleCombinations = list(combinations(range(num_buns), num_required))
 
-    print("Permutations")
-    for i in list(perm):
-        print(i)
-    print("Combinations")
-    for i in list(comb):
-        print(i)
-    print(num_buns, num_required)
+    result = [[] for bunny in range(num_buns)]
+
+    for index in range(len(possibleCombinations)):
+        combination = possibleCombinations[index]
+        for bunny in combination:
+            result[bunny].append(index)
+
+    return result
 
 
 testCase1 = {"num_buns": 2, "num_required": 1}
@@ -24,6 +26,6 @@ testCase2 = {"num_buns": 4, "num_required": 4}
 testCase3 = {"num_buns": 5, "num_required": 3}
 
 
-solution(testCase1["num_buns"], testCase1["num_required"])
-solution(testCase2["num_buns"], testCase2["num_required"])
-solution(testCase3["num_buns"], testCase3["num_required"])
+print(solution(testCase1["num_buns"], testCase1["num_required"]))
+print(solution(testCase2["num_buns"], testCase2["num_required"]))
+print(solution(testCase3["num_buns"], testCase3["num_required"]))
