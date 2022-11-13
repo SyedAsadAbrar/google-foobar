fad5b2c7434618dbb3f1ef375b3b193702eb3646
Asad <syedasadabrar@gmail.com>
2022-11-14 02:12:44 +0500

    escape-pods done

--- 4-2-escape-pods.py
+++ 4-2-escape-pods.py
@@ -1,74 +1,34 @@
 def solution(entrances, exits, path):
     # Your code here
-
     intermediate = [num for num in range(
         len(path)) if (num not in exits and num not in entrances)]
 
-    print("entrances", entrances)
-    print("intermediate", intermediate)
-    print("exits", exits)
-    for arr in path:
-        print(arr)
+    # For visualization purposes
+    # print("entrances", entrances)
+    # print("intermediate", intermediate)
+    # print("exits", exits)
+    # for arr in path:
+    #     print(arr)
 
-    wantToEnter = [0 for arr in path]
+    wantToEnter = [0 for _ in path]
 
-    lastRooms = []
-
-    for roomNumber in exits:
-        for index in range(len(path)):
-            if path[index][roomNumber] > 0 and index not in lastRooms:
-                lastRooms.append(index)
-
-    print("lastRooms", lastRooms)
-
-    for row in range(len(path)):
+    for row in entrances:
         arr = path[row]
         for col in intermediate:
             number = arr[col]
             wantToEnter[col] += number
-
-    print("wantToEnter", wantToEnter)
 
     maxPossible = [sum(
         path[index]) if index in intermediate else 0 for index in range(len(path))]
 
     answer = [num for num in maxPossible]
 
-    print("maxPossible", maxPossible)
-
     for index in range(len(wantToEnter)):
         actualColumnSum = wantToEnter[index]
         maximumPossible = maxPossible[index]
         answer[index] = min(actualColumnSum, maximumPossible)
 
-    print("answer", answer)
-
-    return sum([answer[roomNumber] for roomNumber in lastRooms])
-
-
-def solution2(entrances, exits, path):
-    le = len(entrances)
-    lp = len(path)
-    lx = len(exits)
-
-    print("lengths", le, lp, lx)
-    bunn_count = 0
-    # To find all intermediate rooms
-    inter_paths = path[le:(lp-lx)]
-    print("inter_paths", inter_paths)
-    # Loop through range of length of intermediate rooms
-    for i in range(lp - le - lx):
-        print("i", i)
-        # Sum of an intermediate room's possible number of bunnies allowed
-        sum_range = sum(inter_paths[i])
-        print("sum_range", sum_range)
-        sum_enter = 0                             # Sum of bunnies that enter that room
-        for j in entrances:
-            # Get all bunnies that enter a room
-            sum_enter += path[j][le + i]
-        print("sum_enter", sum_enter)
-        bunn_count += min(sum_enter, sum_range)
-    return bunn_count
+    return sum([answer[roomNumber] for roomNumber in intermediate])
 
 
 testCase1 = {"entrances": [0], "exits": [3], "path": [
