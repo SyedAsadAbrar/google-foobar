50472ba238325550d7b92f6d22ff2b4e6989e334
Asad <syedasadabrar@gmail.com>
2022-12-06 23:32:35 +0500

    Unfinished: Disorderly escape

--- 4-1-disorderly-escape.py
+++ 4-1-disorderly-escape.py
@@ -1,5 +1,5 @@
 from itertools import permutations, combinations
-import collections
+import numpy as np
 
 
 def solution(w, h, s):
@@ -8,26 +8,38 @@ def solution(w, h, s):
     # for i in list(permutations(s * w, w * h)):
     #     print("permutation", i)
 
-    print("s", s)
-
-    print("Combinations")
-
     result_pairs = set()
 
-    set_of_combinations = (set(combinations(s * w * h, w * h)))
+    set_of_combinations = (set(combinations(list(range(s)) * w * h, w * h)))
 
     # list_of_set_of_combinations = list(set_of_combinations)
 
-    print("------")
     for i in set_of_combinations:
-        print(i)
         result_pairs.add(i)
 
-    print("------")
-    # for i in list(result_pairs):
-    #     print(i)
-    # print("------")
+    result_pairs = [np.array(list(i)).reshape((w, h))
+                    for i in list(result_pairs)]
+
+    formatted_pairs = []
+
+    if w != h:
+        if w < h:
+            for i in result_pairs:
+                zeros = np.zeros(h)
+                formatted_pairs.append(np.insert(i, i.shape[0], zeros, axis=0))
+        else:
+            for i in result_pairs:
+                zeros = np.zeros((w, 1))
+                formatted_pairs.append(np.column_stack((i, zeros)))
+
+    if w == h:
+        formatted_pairs = result_pairs
+
+    eigen_values = {tuple(sorted((np.linalg.eigvals(i)))): i for i in formatted_pairs}
+
+    return len(eigen_values)
 
 
-solution(2, 2, [0, 1])
+print(solution(2, 2, 2))
+print(solution(2, 3, 4))
 # solution(2, 3, [0, 1, 2, 3])
